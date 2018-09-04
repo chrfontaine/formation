5508594862667fabf3701926fd3a836dba34226b
christophe <chfontaine.formation@gmail.com>
2018-09-04 16:15:55 +0200

    Ajout des commentaires

--- src/fr/dawan/projet1/Calcul.java
+++ src/fr/dawan/projet1/Calcul.java
@@ -2,6 +2,12 @@ package fr.dawan.projet1;
 
 public class Calcul {
 	
+	/**
+	 * Ajouter deux entiers
+	 * @param a un entier
+	 * @param b un entier
+	 * @return la somme
+	 */
 	public static int ajouter(int a,int b) {
 		return a+b;
 	}
