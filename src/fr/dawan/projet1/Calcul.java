ca5158e8229de4ad31cbfdd36d94b7a72f23caf0
christophe <chfontaine.formation@gmail.com>
2018-09-04 16:10:46 +0200

    Ajout de la classe Calcul

---
+++ src/fr/dawan/projet1/Calcul.java
@@ -0,0 +1,9 @@
+package fr.dawan.projet1;
+
+public class Calcul {
+	
+	public static int ajouter(int a,int b) {
+		return a+b;
+	}
+
+}
--- src/fr/dawan/projet1/Test.java
+++ src/fr/dawan/projet1/Test.java
@@ -5,6 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		//
 		System.out.println("Hello world");
+		System.out.println("Test de l'additon 8+4="+Calcul.ajouter(8,4));
 	}
 
 }
